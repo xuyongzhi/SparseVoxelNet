b369b7a1458db760e3256e2e5448a36d7992ee93
xuyongzhi <buaaxyz@yeah.net>
2018-07-24 11:55:16 +1000

    ju

--- model/regular_pbs/job.sh
+++ model/regular_pbs/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q gpu
 #PBS -l walltime=20:00:00
-#PBS -l mem=16GB
+#PBS -l mem=20GB
 #PBS -l jobfs=0GB
 #PBS -l ngpus=2
 #PBS -l ncpus=6
@@ -19,33 +19,6 @@ module list
  
 
 #------------------------------------------------------------------------
-num_gpus=2
-batch_size=32
-learning_rate0=0.001
-optimizer='adam'
-lr_decay_epochs=20
-lr_decay_rate=0.7
-feed_data='xyzs'
-drop_imo='0_0_5'
-aug_types='N'
-use_bias=1
-block_style='Regular'
-shortcut='MZ'
-loss_lw_gama=-1
-train_epochs=41
-model_flag='m'
-
-residual=0
-
-resnet_size=14
-
-aug_types='rpsfj-360_0_0'
-./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data $drop_imo  $optimizer $use_bias $lr_decay_epochs $lr_decay_rate $resnet_size $block_style $residual $shortcut $loss_lw_gama  $train_epochs
-
-
-aug_types='N'
-./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data $drop_imo  $optimizer $use_bias $lr_decay_epochs $lr_decay_rate $resnet_size $block_style $residual $shortcut $loss_lw_gama  $train_epochs
-
-aug_types='psfj'
-./modelnet.sh   $aug_types  $batch_size $model_flag $learning_rate0 $num_gpus $feed_data $drop_imo  $optimizer $use_bias $lr_decay_epochs $lr_decay_rate $resnet_size $block_style $residual $shortcut $loss_lw_gama  $train_epochs
+./run_modelnet.sh &
+./run_modelnet_1.sh
 
