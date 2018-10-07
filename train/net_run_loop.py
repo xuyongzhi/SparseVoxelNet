e61c8b7cd914e0905490eb25f2bd447945eb2b6e
xuyongzhi <buaaxyz@yeah.net>
2018-10-07 18:35:07 +1100

    add net_main_check

--- train/net_run_loop.py
+++ train/net_run_loop.py
@@ -377,6 +377,37 @@ def net_model_fn( features, labels, mode, model_class,
           eval_metric_ops=metrics)
 
 
+def net_main_check(
+    flags_obj, model_function, input_function, net_data_configs, shape=None):
+  from datasets.tfrecord_util import get_ele
+  import numpy as np
+  dset_shape_idx = net_data_configs['dset_shape_idx']
+
+  with tf.Graph().as_default():
+    def input_fn_train(num_epochs):
+      return input_function(
+          is_training=True, data_dir=flags_obj.data_dir,
+          batch_size=distribution_utils.per_device_batch_size(
+              flags_obj.batch_size, flags_core.get_num_gpus(flags_obj)),
+          num_epochs=num_epochs,
+          num_gpus=flags_core.get_num_gpus(flags_obj),
+          examples_per_epoch = flags_obj.examples_per_epoch
+          )
+    dataset = input_fn_train(1)
+    ds_iterator = dataset.make_one_shot_iterator()
+    features, labels = ds_iterator.get_next()
+
+    with tf.Session() as sess:
+      features_, labels_ = sess.run([features, labels])
+      vidx_per_face = get_ele(features_, 'vidx_per_face', dset_shape_idx)
+      fidx_per_vertex = get_ele(features_, 'fidx_per_vertex', dset_shape_idx)
+
+      vpf_min = np.min(vidx_per_face)
+      fpv_min = np.min(fidx_per_vertex)
+      lb_min = np.min(labels_)
+      import pdb; pdb.set_trace()  # XXX BREAKPOINT
+      pass
+
 def net_main(
     flags_obj, model_function, input_function, net_data_configs, shape=None):
   """Shared main loop for ResNet Models.
