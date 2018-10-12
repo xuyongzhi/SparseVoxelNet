5abc470c15e032068ef156a8abaebd29c3407469
xuyongzhi <buaaxyz@yeah.net>
2018-10-12 19:15:45 +1100

    ju

--- train/run_train.sh
+++ train/run_train.sh
@@ -1,16 +1,17 @@
-export CUDA_VISIBLE_DEVICES=1
+#export CUDA_VISIBLE_DEVICES=1
 
 net_flag='8A'
-ng=1
-bs=1
+ng=2
+bs=2
 rs=--rs
 normxyz='raw'
 normedge='raw'
 
-normxyz='min0'
 
 ipython  ./train_main.py -- --num_gpus $ng --net_flag $net_flag --batch_size $bs   $rs --normxyz $normxyz --normedge $normedge
 
+normxyz='min0'
+ipython  ./train_main.py -- --num_gpus $ng --net_flag $net_flag --batch_size $bs   $rs --normxyz $normxyz --normedge $normedge
 
 
 
